d5c5c0e994b970d794dd65ef38c58312f4572b91
naufal172 <naufalfauzi58@gmail,com>
2021-10-27 20:52:39 +0700

    Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Toko barang1 = new Toko(1, "N0001", "JNE", 21000, "Dropship1", "Surabaya", "Bejo", "DayeuhKolot", true, true, 40000);
+
+       Toko barang2 = new Toko(2, "N0003", "J&T", 21000, "Eiger", "Bandung Kopo", "Tini", "DayeuhKolot", true, 40000);
+        Toko barang3 = new Toko(3, "N0004", "Pos Indonesia", 23000, "Eiger", "Bandung Kopo", "Bambang","DayeuhKolot");
+
+        Toko barang4 = new Toko(4, "N0002", "Si Cepat", 24000, "Dropship2", "Pontianak", "Joko","DayeuhKolot", true);
+
+    }
+
+}
